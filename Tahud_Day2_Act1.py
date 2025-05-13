ade48ca4f47b83563e10a4c20f7b75a6ade59fab
honeylyn2002 <93508285+Gebsi@users.noreply.github.com>
2025-05-13 17:49:58 +0800

    Add files via upload

---
+++ Tahud_Day2_Act1.py
@@ -0,0 +1,13 @@
+x, a, s, s2 = 5, 5.75, "100", "45.67"
+
+y = float(x)
+print(f"Integer x: {x}, After casting to float: {y}")
+
+b = int(a)
+print(f"Float a: {a}, After casting to integer: {b}")
+
+num = int(s)
+print(f"String s: {s}, After casting to integer: {num}")
+
+num2 = float(s2)
+print(f"String s2: {s2}, After casting to float: {num2}")
